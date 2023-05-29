4cd9f4925ce8f960e21c562c499ed1c20a839fcc
Tomas Duclos <duclosiw@gmail.com>
2023-05-29 19:14:54 -0400

    Input component

---
+++ src/components/form/Input.tsx
@@ -0,0 +1,70 @@
+import { useState, useRef } from 'react'
+import { HiEye, HiEyeSlash } from 'react-icons/hi2'
+
+interface Props {
+  type?: 'text' | 'password'
+  placeholder: string
+  name: string
+}
+
+const iconClassName = 'w-5 h-5'
+
+export default function Input ({
+  type = 'text',
+  placeholder,
+  name
+}: Props) {
+  const inputRef = useRef<HTMLInputElement>(null)
+  const [isFocus, setIsFocus] = useState<boolean>(false)
+  const [showPassword, setShowPassword] = useState<boolean>(false)
+
+  const handleToggleFocus = (e: React.FocusEvent<HTMLInputElement>) => {
+    const { value } = e.target
+
+    if (value !== '') {
+      setIsFocus(true)
+      return
+    }
+
+    setIsFocus((prev) => !prev)
+  }
+
+  const handleMouseDownTogglePassword = () => {
+    setShowPassword((prev) => !prev)
+    setTimeout(() => {
+      inputRef.current?.focus()
+    }, 0)
+  }
+
+  return (
+    <label className="block bg-[#EDEDED] hover:bg-gray-200 transition-all rounded relative flex focus-within:bg-white focus-within:ring-2 focus-within:ring-black">
+      <input
+        ref={inputRef}
+        className="peer pt-5 pb-2 px-2 bg-transparent focus:outline-none w-full font-semibold"
+        type={showPassword ? 'text' : type}
+        name={name}
+        onFocus={handleToggleFocus}
+        onBlur={handleToggleFocus}
+        autoComplete="off"
+      />
+
+      <span className={`uppercase text-[#979797] font-bold absolute transition-all ${isFocus ? 'left-2 top-1 text-[9px]' : 'left-4 top-1/2 -translate-y-1/2 text-xs'}`}>{placeholder}</span>
+
+      {
+        (type === 'password' && isFocus) && (
+          <button
+            className="min-w-[3rem] max-w-[3rem] place-items-center hidden peer-focus:grid"
+            type="button"
+            onMouseDown={handleMouseDownTogglePassword}
+          >
+            {
+              showPassword
+                ? <HiEyeSlash className={iconClassName} />
+                : <HiEye className={iconClassName} />
+            }
+          </button>
+        )
+      }
+    </label>
+  )
+}
--- src/screens/Auth/Step_0/index.tsx
+++ src/screens/Auth/Step_0/index.tsx
@@ -1,3 +1,5 @@
+import { Input } from 'components/form'
+
 export default function Step0 () {
   return (
     <div className="bg-white h-full flex">
@@ -6,7 +8,19 @@ export default function Step0 () {
           className="w-40 mx-auto"
           src="/logo.png"
         />
-        LEft
+
+        <form className="flex flex-col gap-4">
+          <Input
+            placeholder="Username"
+            name="username"
+          />
+
+          <Input
+            type="password"
+            placeholder="Password"
+            name="password"
+          />
+        </form>
       </div>
 
       <div className="bg-yellow-500 w-full bg-stone-900"></div>
